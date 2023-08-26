9422eb5c1e9407aa69822cfb821fe1bbc51abd17
Barbara Helen <dsbarbarahelen@gmail.com>
2023-08-25 21:51:51 -0400

    Finalização do app, utilizando Randomisation.

--- DiceGame/DiceGame/ViewController.swift
+++ DiceGame/DiceGame/ViewController.swift
@@ -13,18 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var rollButton: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
         
-        diceImageView1.image = UIImage(named: "DiceSix")
-        diceImageView2.image = UIImage(named: "DiceTwo")
-        
-    }
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
     @IBAction func rollButtonPressed(_ sender: Any) {
         
-        diceImageView1.image = UIImage(named: "DiceFour")
-        diceImageView1.image = UIImage(named: "DiceFour")
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
     }
     
 }
