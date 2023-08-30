ace447f7be8d42aa94991c2a76f2d52d6683d84b
Barbara Helen <dsbarbarahelen@gmail.com>
2023-08-29 22:53:21 -0400

    Criação do projeto no XCode.

---
+++ MagicBall/MagicBall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicBall
+//
+//  Created by Barbara Silva on 2023-08-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
